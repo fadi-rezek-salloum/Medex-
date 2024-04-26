17cf0f20ea4a56016785f8af703d11e61ba452fe
Fadi Rezek Salloum <fadirezeksalloum.dev@gmail.com>
2024-04-26 17:31:04 +0200

    Added dynamic category

--- advertisement/models.py
+++ advertisement/models.py
@@ -2,6 +2,7 @@ from common.utils.file_upload_paths import ads_thumbnail_images_path
 from common.validators.image_extension_validator import image_extension_validator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from product.models import Category
 
 
 class Advertisement(models.Model):
@@ -13,5 +14,17 @@ class Advertisement(models.Model):
         validators=[image_extension_validator],
     )
 
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True, blank=True, default=None
+    )
+
+    is_featured = models.BooleanField(default=False)
+
     def __str__(self):
         return self.thumbnail.url
+
+    def save(self, *args, **kwargs):
+        if not self.category:
+            default_category, _ = Category.objects.get_or_create(name="generic")
+            self.category = default_category
+        super().save(*args, **kwargs)
--- advertisement/serializers.py
+++ advertisement/serializers.py
@@ -1,9 +1,12 @@
+from product.serializers import CategorySerializer
 from rest_framework import serializers
 
 from .models import Advertisement
 
 
 class AdvertisementSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
     class Meta:
         model = Advertisement
         fields = "__all__"
